e7d711f40fc9170d9453bf39c54ab8a823cab2c9
Yao Noel ACHI <achi.noel@hotmail.com>
2022-11-27 22:09:09 -0500

    feat: add shared network

--- platform/envs/dev/variable.tf
+++ platform/envs/dev/variable.tf
@@ -7,3 +7,24 @@ variable "tf_bucket_project" {
   description = "Project used to host terraform buckets"
   type        = string
 }
+
+variable "host_network_name" {
+  description = "Name of the host shared vpc"
+  type        = string
+  default     = "shared-vpc"
+}
+
+variable "shared_project_id" {
+  description = "Shared project ID"
+  type        = string
+}
+
+variable "shared_vpc_svc_projects" {
+  description = "List of service projects ID"
+  type        = list(string)
+}
+
+variable "shared_vpc_subnets" {
+  description = "subnets for the shared vpc"
+  type        = list(any)
+}
